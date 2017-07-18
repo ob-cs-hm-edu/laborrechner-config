aa84ff5ecd17840422dedd7ad6bf826cb76571c5
Oliver Braun <ob@cs.hm.edu>
2017-07-18 09:38:55 +0200

    Update haskell-powershell.bat

--- haskell-powershell.bat
+++ haskell-powershell.bat
@@ -1,3 +1,11 @@
-$env:Path += ";C:\Program Files\Haskell Platform\8.0.2-a\bin;H:\Benutzer\AppData\local\bin\"
+
+Clear-Host
+$HPLocation ="C:\Program Files\Haskell Platform\8.0.2-a\bin"
+$LocalHPLocation = "H:\Benutzer\AppData\local\bin"
+$Reg = "Registry::HKLM\System\CurrentControlSet\Control\Session Manager\Environment"
+$OldPath = (Get-ItemProperty -Path "$Reg" -Name PATH).Path
+$NewPath= $OldPath + ';' + $HPLocation + ';' + $LocalHPLocation
+Set-ItemProperty -Path "$Reg" -Name PATH –Value $NewPath
+
 stack config set system-ghc --global true
 
