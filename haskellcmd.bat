e8fb5f0d85069030029c19964cb5dde18f0eeb8f
Oliver Braun <ob@cs.hm.edu>
2017-07-18 09:47:31 +0200

    Update haskellcmd.bat

--- haskellcmd.bat
+++ haskellcmd.bat
@@ -1,3 +1,3 @@
+set PATH="%PATH%;C:\Program Files\Haskell Platform\8.0.2-a\bin;H:\Benutzer\AppData\local\bin\"
 stack config set system-ghc --global true
-set PATH=%PATH%;H:\Benutzer\AppData\local\bin\
 cmd
