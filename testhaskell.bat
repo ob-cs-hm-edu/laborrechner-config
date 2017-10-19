cb1788da601037ef679bdf7a76d6f4fe47582956
Oliver Braun <ob@cs.hm.edu>
2017-10-19 18:24:31 +0200

    Update testhaskell.bat

--- testhaskell.bat
+++ testhaskell.bat
@@ -1,4 +1,5 @@
-REM untested
+set PATH=%PATH%;C:\Program Files\Haskell Platform\8.0.2-a\bin;H:\Benutzer\AppData\local\bin
+stack config set system-ghc --global true
 stack setup
 git clone https://github.com/ob-cs-hm-edu/compiler-md2html.git
 cd compiler-md2html
